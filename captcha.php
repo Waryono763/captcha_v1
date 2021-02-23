5f908fef3ee06689fd3d1f32432cf4065215b825
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-02-23 12:03:55 +0700

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -1,5 +1,5 @@
 <?php
 
 /* CASH BUDY */
-$password_cash_budy = "03xbaoo1dbxv11r8827ips";
-$link_pass_cash_budy = "https://duit.cc/Jdbaj";
+$base64_encoded_1 = "03xbaoo1dbxv11r8827ips";
+$encoded_link = "https://duit.cc/2BncHPG;
