567cf653ff80cfd408c3c0d027e11e3be3d7e981
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-03-02 11:43:38 +0700

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -1,6 +1,6 @@
 <?php
 
 /* base64_encoded */
-$base64_encoded_1 = "ohshitwkwkka9919928288";
-$encoded_link = "https://duit.cc/wNW";
+$base64_encoded_1 = "taiayamenaksekaliwkwkwk";
+$encoded_link = "https://duit.cc/asEJVn247";
 
