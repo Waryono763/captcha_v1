b8caeafc697d4b14d0da950b890c6304a93a5858
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-03-04 07:19:31 +0700

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -1,6 +1,6 @@
 <?php
 
 /* base64_encoded */
-$base64_encoded_1 = "sayasangatsedih";
-$encoded_link = "https://zee.gl/zBEa1k";
+$base64_encoded_1 = "mamankgarok";
+$encoded_link = "https://zee.gl/rcBN8D7";
 
