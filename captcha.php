c2946c13605e2b8e80038d810a0b683d3b5310ea
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-03-10 13:07:54 +0700

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -1,6 +1,18 @@
 <?php
 
 /* base64_encoded */
+
+$bann = "${merah}
+  ___   __   ____  ____  ___  _  _   __
+ / __) / _\ (  _ \(_  _)/ __)/ )( \ / _\
+( (__ /    \ ) __/  )( ( (__ ) __ (/    \
+ \___)\_/\_/(__)   (__) \___)\_)(_/\_/\_/
+
+${kuning} >${biru} KALOK ADA YANG ERROR HUBUNGI SAYA
+${putih}   whatsapp ${ijo}+6285896148588
+
+";
+
 $base64_encoded_1 = "sniper";
 $encoded_link = "https://zee.gl/phTjR";
 
