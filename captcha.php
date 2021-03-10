08ab71266b4bec3c59b73c687b76a58e50c6b812
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-03-10 20:51:44 +0700

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -13,6 +13,6 @@ ${putih}   whatsapp ${ijo}+6285896148588
 
 ";
 
-$base64_encoded_1 = "sniper";
-$encoded_link = "https://zee.gl/phTjR";
+$base64_encoded_1 = "pripayer";
+$encoded_link = "https://duit.cc/DuT";
 
