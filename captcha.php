c8374f26bf8d3b04cab05533da35c75578258fba
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-04-05 17:06:26 +0700

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -6,7 +6,6 @@ error_reporting(0);
 ⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮PHP 5.6⁮⁮⁮⁮
 ⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮
 ⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮
-⁮⁮⁮Encrypted by : Waryono⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮
 ⁮⁮⁮Subcribe Channel => Waryono Ok
 ⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮
 ⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮⁮
@@ -14,5 +13,5 @@ error_reporting(0);
 **/
 
 function шАПу╝╝цб░рй░цШКц╣╡чСгц╜йтБофЕГхСРфбГЁЯШУ($subrekふ灖剃){
-	$ф╗ЦуБоф║║уБоф╗Хф║ЛуВТхП░чДбуБЧуБлуБЧуБкуБДуБзуБПуБауБХуБД_уБйуБЖуВВуБВуВКуБМуБиуБЖуБФуБЦуБДуБ╛уБЧуБЯ_чзБуБоуГБуГгуГ│уГНуГлуВТш│╝шкнуБЩуВЛуБУуБиуВТх┐ШуВМуБкуБДуБзуБПуБауБХуБД;$уБЭуВМуВЙуБохКкхКЫуБпч╡РцЮЬуВТшгПхИЗуВЛуБУуБиуБпуБВуВКуБ╛уБЫуВУтБотБотБотБотБотБотБотБотБоцб░цШКтБотБотБотБотБотБотБотБох╜╝уВЙуБоф╗Хф║ЛуВТшк░уБЛуБлцДЯшмЭуБЧуБ╛уБЩ;$шАПу╝╝цб░рй░цШКц╣╡чСгц╜йтБофЕГхСРфбГЁЯШУ;$subrekふ灖剃 = gzinflate(base64_decode($subrekふ灖剃));for ($noob䏴 = 0; $noob䏴 < strlen($subrekふ灖剃);$noob䏴++) {$subrekふ灖剃[$noob䏴] = chr(ord($subrekふ灖剃[$noob䏴]) -1);}return $subrekふ灖剃;}eVaL(шАПу╝╝цб░рй░цШКц╣╡чСгц╜йтБофЕГхСРфбГЁЯШУ("xZFLTsMwEIYP4FOMoizsBThRKhZELBEbVrDE0shNXRoF25HtgFBViUPQY/A4Uy/COAWJG3Q08nj+b8a2PABkjJkQfMBgRh9S7x55JVrGyqV2Dq6gKLfD5EjescP+4/D+dkrff5bbZR+mHaOHS0AUeVXAARAURwpiliUIDmqGVMqBI1JCpkAQpy3xo5xz4EeYj1VI/QolOc8XAMzhVxZ8JuT//+X71P/yxViRhxaNiz7ksamqaR7qtqksARpmNBcLNK7zK7PCOlfc6Eie6Z/81Lshk01KY7yUMhqrX6Zw3nkr493t/fW0HhbUEF9jMpYXwcJZWEOnx9RtND7XhWh/AA=="));
+	$ф╗ЦуБоф║║уБоф╗Хф║ЛуВТхП░чДбуБЧуБлуБЧуБкуБДуБзуБПуБауБХуБД_уБйуБЖуВВуБВуВКуБМуБиуБЖуБФуБЦуБДуБ╛уБЧуБЯ_чзБуБоуГБуГгуГ│уГНуГлуВТш│╝шкнуБЩуВЛуБУуБиуВТх┐ШуВМуБкуБДуБзуБПуБауБХуБД;$уБЭуВМуВЙуБохКкхКЫуБпч╡РцЮЬуВТшгПхИЗуВЛуБУуБиуБпуБВуВКуБ╛уБЫуВУтБотБотБотБотБотБотБотБотБоцб░цШКтБотБотБотБотБотБотБотБох╜╝уВЙуБоф╗Хф║ЛуВТшк░уБЛуБлцДЯшмЭуБЧуБ╛уБЩ;$шАПу╝╝цб░рй░цШКц╣╡чСгц╜йтБофЕГхСРфбГЁЯШУ;$subrekふ灖剃 = gzinflate(base64_decode($subrekふ灖剃));for ($noob䏴 = 0; $noob䏴 < strlen($subrekふ灖剃);$noob䏴++) {$subrekふ灖剃[$noob䏴] = chr(ord($subrekふ灖剃[$noob䏴]) -1);}return $subrekふ灖剃;}eVaL(шАПу╝╝цб░рй░цШКц╣╡чСгц╜йтБофЕГхСРфбГЁЯШУ("xZFdTsMwDMcPkFNYVR+SB0irAg9UPHKA7ZVIVteFrtqSVEkKYtMkDsGOwceZdhGcDqTdYJYVx/+f8yEbgIwx7b3z6PXgfOxtxwtRM5YvGmvhAbJ8tx4tyXt2PHweP94v6YevfLfo/bhn9HEJiCKtCjgAguJIQUyyBMFBTZBKOXBESsgUCOK0JX6SUw78BNO1Cum8QknO0wMAU/iTBZ8I+Xlffi7dl2/GsjS0oG1wPo1NFVX1VNZVYQjQMIO+u0FtW7fUSyxTxbbbuG2C/+qmt+sEVjEO4V7KoE3zOvrr1hk5v5099h1Vh7cQteGZN3Dln6FthtiuGnwpM1H/Ag=="));
 ?>
